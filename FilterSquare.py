0d3eb5870f1d0a1ef856e39a74f5b35ef9b157bd
Joshua Sun <joshua.sun@mikara.co>
2019-11-25 21:39:20 +1100

    Added filtering to remove busses in the wrong lane

--- FilterSquare.py
+++ FilterSquare.py
@@ -1,11 +1,14 @@
 import numpy as np
 
-def FilterSquare(bbox = []):
+def FilterSquare(bbox = [], point = []):
     length = bbox[2] - bbox[0]
     breadth = bbox[3] - bbox[1]
 
     if abs(length/breadth) < 1.2: 
         return False
-    else:
-        return True
+    
+    if point[1] < bbox[1] or point[1] > bbox[3]:
+        return False
+    
+    return True
 
